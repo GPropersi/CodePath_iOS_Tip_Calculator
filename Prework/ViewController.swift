f3f7d02899152d470b0ca3a6814baa854070c2e9
Giovanni Propersi <giovannigp@gmail.com>
2021-12-28 23:12:54 -0800

    Added validation to bill input textfield. No more than one period, no more than two digits after period.

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -8,7 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension String {
+    //https://stackoverflow.com/questions/31746223/number-of-occurrences-of-substring-in-string-in-swift
+    func numberOfOccurrencesOf(string: String) -> Int {
+        return self.components(separatedBy:string).count - 1
+    }
+}
+
+class ViewController: UIViewController, UITextFieldDelegate {
     
     let defaults = UserDefaults.standard
     let USER_DEFINED_TIP = "UserDefinedTip"
@@ -21,8 +28,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipPercentOutput: UILabel!
     @IBOutlet weak var maxSliderLabel: UILabel!
     
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        billAmountTextField.delegate = self
         
         self.title = "Tip Calculator"
         billAmountTextField.becomeFirstResponder()
@@ -42,6 +53,16 @@ class ViewController: UIViewController {
         
     }
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Add $ sign to bill input text
+        let current_text_field = textField
+        current_text_field.text = "$"
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("Hit this")
+    }
+    
     @IBAction func get_tip_perc(_ sender: Any) {
         // Capture the user chosen percentage from the slider
         // Recalculates tip/bill based on new chosen tip percentage
@@ -59,8 +80,27 @@ class ViewController: UIViewController {
         // Calculates the tip based on the total Bill
         // and selected tip amount. Responds directly to editing
         // of the bill amount.
+        var bill_amount = billAmountTextField.text!
+        let period_occ: Int = bill_amount.numberOfOccurrencesOf(string: ".")
         
-        let bill = Double(billAmountTextField.text!) ?? 0
+        if period_occ > 1 {
+            // Not allow user to enter more than one period
+            billAmountTextField.text = String(bill_amount.dropLast(1))
+            return
+        }
+        
+        // Create array with strings before and after period. User should not be inputting
+        // values with more than two digits after period. Check for this.
+        let strings_with_period = bill_amount.split(separator: ".")
+        if strings_with_period.count > 1 && strings_with_period[1].count > 2{
+            billAmountTextField.text = String(bill_amount.dropLast(1))
+            return
+        }
+        
+        bill_amount = bill_amount.replacingOccurrences(of: "$", with: "")
+        bill_amount = bill_amount.replacingOccurrences(of: ".", with: "")
+        
+        let bill = Double(bill_amount) ?? 0
         
         // Round tip percent to 2 decimal digits
         let tip_percent = round(Double(tipPercentSlider.value) * 100) / 100.0
